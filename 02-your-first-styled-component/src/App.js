c64fb91d8397f59f0773a4b9683840688786bd35
Dustin Schau <dustinschau@gmail.com>
2018-05-06 23:46:48 -0500

    chore: your first styled component

---
+++ 02-your-first-styled-component/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Button from './components/Button'; // TODO: make a styled component
+
+class App extends Component {
+  render() {
+    return (
+      <Button />
+    );
+  }
+}
+
+export default App;
