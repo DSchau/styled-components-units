fd914aab51bcde6ec7c6e463e7a686a452996f4c
Dustin Schau <dustinschau@gmail.com>
2018-05-29 15:19:44 -0500

    chore: minor tweak

--- 07-usage-with-existing-libraries/src/components/Alert.js
+++ 07-usage-with-existing-libraries/src/components/Alert.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div.attrs({
-  className: props => (props.type ? `alert alert-${props.type}` : 'alert')
-})``;
+const Container = styled.div.attrs({})``;
 
 export default function Alert({ children, ...rest }) {
   return <Container {...rest}>{children}</Container>;
